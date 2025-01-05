7fe83527016765f1f9d5042aef8f32d5d52ffae1
jigyanshrathore <122160447+jigyanshrathore@users.noreply.github.com>
2025-01-05 18:36:19 +0530

    Update urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register/', views.register_user, name='register_user'),
+    path('create/', views.create_task, name='create_task'),
     path('', views.get_all_tasks, name='get_all_tasks'),
     path('<int:pk>/', views.get_task_by_id, name='get_task_by_id'),
     path('<int:pk>/update/', views.update_task, name='update_task'),
     path('<int:pk>/delete/', views.delete_task, name='delete_task'),
-    path('create/', views.create_task, name='create_task'),
 ]
